218f8d49258f57f3dc4722a149db431f5240b848
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-25 23:11:35 +0530

    project

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output  "public_ip" {
   description = "public IP of the instance created"
 }
 
+output  "private_ip" {
+  value       = aws_instance.this.private_ip
+  description = "private IP of the instance created"
+}
+
