f8b538dca6527349f370f486234512b4c407f16d
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-25 23:12:44 +0530

    project

--- variables.tf
+++ variables.tf
@@ -4,3 +4,12 @@ variable "ami_id" {
   description = "this is the AMI used for creating EC2 instance"
 }
 
+variable "instance_type" {
+  type = string
+  description = "Instance type used for creating EC2 instance"
+  validation {
+    condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+    error_message = "Please select either t3 micro or small or medium"
+  }
+}
+
