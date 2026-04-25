71a8f619aefa98532c4e7734094bd4cb80e01885
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-25 23:11:15 +0530

    project

--- ec2.tf
+++ ec2.tf
@@ -1,20 +1,6 @@
-
-resource "aws_instance" "this"{
-    # ami = "ami-0cae6d6fe6048ca2c"
-    ami = var.ami_id  #mandatory
-    # instance_type = "t3.micro"
-    instance_type = var.instance_type  #mandatory
-    vpc_security_group_ids = var.sg_ids  #mandatory
-
-    # tags = {
-    #   Name = "terraform"
-    #   Terraform = "true"
-    # }
-
-    tags = var.tags #optional
+resource "aws_instance" "this" {
+    ami = var.ami_id # mandatory
+    instance_type = var.instance_type # mandatory
+    vpc_security_group_ids = var.sg_ids # mandatory
+    tags = var.tags # optional
 }
-
-
-# deenni manam genaral gaani main gaani pettali this not a mandatory its a recommendation
-
-#aws_instance pakkana this or main pettali terraform ane word badhulu...
